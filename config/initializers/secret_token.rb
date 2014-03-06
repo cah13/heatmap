f3f721dbab42339b51ccb0484089585de1c6d4c0
My Name <myemail@example.com>
2014-03-06 13:32:22 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heatmap::Application.config.secret_key_base = '10660306821af23c3c1761897c51f9b4109688c8ec072e9bc991793f8fab92cb6f351df5515dfd916d64c628493cdc71e8a465a8274561f120ddb51437aafb61'
