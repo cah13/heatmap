392b9f79666da1d68650241dd8918adeb23c320b
My Name <myemail@example.com>
2014-03-06 13:44:00 -0500

    homepage initialized

---
+++ spec/features/user_can_goto_homepage.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "user can land on homepage" do
+  it "will land user on haomepage" do
+    
+    # Setup
+
+    # Workflow
+
+    # Expecatations
+    expect(page).to have_link "Home"
+
+  end
+end
