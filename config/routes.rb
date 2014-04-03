8eff58e16d970ab4cd638115e2913e4ce23e8de2
My Name <myemail@example.com>
2014-04-03 14:48:18 -0400

    bootstrap and jquery installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Heatmap::Application.routes.draw do
 
+resources :users
+
 root "welcome#index"
+
+get "/login", to: "session#new"
+post "/session", to: "session#create"
+delete "/session", to: "session#destroy"
 
 end
