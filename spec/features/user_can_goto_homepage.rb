9451fb1669bb653f6ab077e86f0a814571ffae08
My Name <myemail@example.com>
2014-04-03 13:56:59 -0400

    added user model and db setup

--- spec/features/user_can_goto_homepage.rb
+++ spec/features/user_can_goto_homepage.rb
@@ -4,11 +4,15 @@ describe "user can land on homepage" do
   it "will land user on haomepage" do
     
     # Setup
-
+    visit "/"
     # Workflow
 
     # Expecatations
-    expect(page).to have_link "Home"
-
+    expect(page).to have_content "Home"
+    expect(page).to have_content "Sales"
+    expect(page).to have_content "Rentals"
+    expect(page).to have_content "Login"
+    expect(page).to have_content "Signup"
+    expect(page).to have_content "Contact Us"
   end
 end
